204cad116d6a7887ac996ee28365a4f1c18fae80
Thomas <thomaslee9474@gmail.com>
2019-07-12 13:10:11 -0700

    [removed] directory

---
+++ vertex.py
@@ -0,0 +1,41 @@
+#!python
+
+""" Vertex Class
+A helper class for the Graph class that defines vertices and vertex neighbors.
+"""
+
+
+class Vertex(object):
+
+    def __init__(self, vertex):
+        """initialize a vertex and its neighbors
+
+        neighbors: set of vertices adjacent to self,
+        stored in a dictionary with key = vertex,
+        value = weight of edge between self and neighbor.
+        """
+        self.id = vertex
+        self.neighbors = {}
+
+    def addNeighbor(self, vertex, weight=0):
+        """add a neighbor along a weighted edge"""
+        # TODO check if vertex is already a neighbor
+        # TODO if not, add vertex to neighbors and assign weight.
+
+    def __str__(self):
+        """output the list of neighbors of this vertex"""
+        return str(self.id) + " adjancent to " +
+        str([x.id for x in self.neighbors])
+
+    def getNeighbors(self):
+        """return the neighbors of this vertex"""
+        # TODO return the neighbors
+
+    def getId(self):
+        """return the id of this vertex"""
+        return self.id
+
+    def getEdgeWeight(self, vertex):
+        """return the weight of this edge"""
+        # TODO return the weight of the edge from this
+        vertext to the given vertex.
